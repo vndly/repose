40df37d7d22945463d847536ac2e6c009d33ec5b
Mauricio Togneri <mtogneri@openwt.com>
2016-06-07 17:13:35 +0200

    Updated server config

--- src/main/java/com/mauriciotogneri/repose/kernel/ServerConfig.java
+++ src/main/java/com/mauriciotogneri/repose/kernel/ServerConfig.java
@@ -1,21 +1,16 @@
 package com.mauriciotogneri.repose.kernel;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 public final class ServerConfig
 {
     public final String hostName;
     public final int port;
 
-    public ServerConfig(String configPath) throws IOException
+    public ServerConfig(String hostName, String port) throws IOException
     {
-        Properties properties = new Properties();
-        properties.load(new FileInputStream(configPath));
-
-        this.hostName = properties.getProperty("host");
-        this.port = Integer.parseInt(properties.getProperty("port"));
+        this.hostName = hostName;
+        this.port = Integer.parseInt(port);
     }
 
     public String host()
